f4585873a41969cf24a026e5ec2ccd7ab3b3cad5
William Luther Zambo <demowillzam123@gmail.com>
2023-11-09 01:53:21 +0800

    Enforcing the permission and updating deprecated methods

--- app/src/main/java/com/teamlitiaina/tragohelper/fragment/MapFragment.kt
+++ app/src/main/java/com/teamlitiaina/tragohelper/fragment/MapFragment.kt
@@ -11,7 +11,6 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.location.Location
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.util.Patterns
@@ -19,10 +18,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
